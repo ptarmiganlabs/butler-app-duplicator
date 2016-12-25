8a60161bc744b80a7fafd5a58c3450f48faec7fa
mountaindude <mountaindude@ptarmiganlabs.com>
2016-12-25 09:35:39 +0100

    Added README, new enpoint
    Added full documentation in README.md
    Added new duplicateKeepScript endpoint

--- README.md
+++ README.md
@@ -1,2 +1,143 @@
 # sense-template-app-duplicate
 Node.js server for handling back-end tasks related to creating new Sense apps, using existing apps as templates
+
+
+
+## Why an app duplication service?
+
+While perfectly possible to create every Sense app from scratch, Sense developers quickly realise the value of having a library of 
+code snippets, usually in the form of include files that can be brought into the load script by means of $(Must_Include=[lib://path/to/script.qvs]) or similar.
+This does go a long way towards reusing important pieces of code, but it also requires that developers remember to include the correct source code files.
+It also helps when training new Sense developers - they do not have to learn every detail about every piece of code that is shared across apps - the include
+files provide a nice layer of abstraction.
+  
+This app duplicator project started after realising that the above could be improved.
+
+
+## Benefits of an app duplication service
+
+* **Lowered barrier of entry for new Sense developers**  
+Sense app development can be a challenge for people coming from other platforms and languages.  
+The script language borrows from other languages, but is at the end of the day unique.
+By allowing developers coming to the Sense platform   
+to easily create skeleton apps based on well designed and complete template apps, these people will become productive way quicker than otherwise.  
+Their apps will also be of higher quality than if they had to start over from scratch for each app they create.
+
+* **Productivity boost for experienced Sense developers**  
+Experienced developers are often faced with similar challenges over and over again.  
+Extracting data into a QVD layer is a classic example - these apps often look about the same over and over again.  
+Creating a template out of such an app, that template can then be used every time a QVD extractor app is needed.  
+Each such app will be structured the same way, do error reporting the same way, use the same variable names etc.    
+The new app usually still needs to be customised to the exact task at hand - but in general the result is simply better, more readable code.
+
+* **Better odds of apps aligning with corporate coding standards**  
+If there are more than a handful och Sense developers in a company, there is a good chance there are some guidelines on how to structure the load scripts in Sense apps.  
+By instantiating new apps from template apps (that follow the coding standards), better code quality will be achieved.
+
+# Requirements and installation
+* The app duplicator service is intended to be used in a Sense Enterprise environment. Using it with Sense Desktop might be possible, but will require modifications to the source code.
+* The service can be run on a Sense server, or on some other server. The only condition is that the server where the app duplicator service runs must be able to connect to the Sense servers (of course..).
+* Node.js must be installed.
+* Download and extract the app duplicator code to a suitable directory on the server.
+* Copy the default_config.json file to a new file called default.json. Place the new file in the "config" directory.
+* Edit the default.json file to meet your specific environment.
+* Start the service by running "node index.js" from the command prompt.
+* When new apps are created, their reload scripts are either left intact, or replaced with a new script (there are two different REST endpoints for this).  
+If the script is replaced, the new script is retrieved from a URL - typically from a revision control system system such as Github.  
+* Create a custom property (by using the Sense QMC) called "AppIsTemplate". It should only exist for Apps. Possible values should be "Yes" and "No".
+
+
+# Usage
+Before using the app duplicator service, you need to specify what existing apps should be considered to be templates.
+Open the template apps in the QMC's app section, then set the custom property AppIsTemplate to "Yes" for these apps.  
+That's it - no other configuration needed to make a template out of an existing app!
+
+## Retrieve a list of template apps
+You can use any tool capable of creating REST calls to test the service, including a simple web browser.  
+Standard curl is used below, the assumption is that the command is executed on the same server where the app duplicator service is running.
+
+```sh
+curl -X GET "http://localhost:8000/getTemplateList"
+```
+
+Returns a HTTP 200 response, with a json structure such as
+
+```json
+[{
+    "name": "SQL Server to QVD - TEMPLATE",
+    "id": "11111111-2222-3333-4444-555555555555",
+    "description": "Template app for mirroring result sets coming from SQL Server to the Sense server, where they are stored locally on disk as QVDs."
+}, {
+    "name": "Postgres to QVD - TEMPLATE",
+    "id": "21111111-2222-3333-4444-555555555555",
+    "description": "Template app for mirroring result sets coming from Postgres to the Sense server, where they are stored locally on disk as QVDs."
+}, {
+    "name": "Skeleton UI app - TEMPLATE",
+    "id": "31111111-2222-3333-4444-555555555555",
+    "description": "Template app for creating user facing Sense apps. The app loads shared code from include files, including error handling and code for notifying end users when reloads are completed (and new data is available in the app)."
+}]
+``` 
+  
+## Create a new app based on a template app, replacing the load script of the new app
+When creating a new app that should get its load script from the file specified in the config file's loadScriptURL option, use the /duplicateNewScript endpoint.  
+The new app will be reloaded (using the new load script) before saved to disk.
+  
+```sh
+curl -X GET "http://localhost:8000/duplicateNewScript?appName=My%20new%20app&templateAppId=11111111-2222-3333-4444-555555555555&ownerUserId=joe"
+```
+
+Returns
+
+```
+HTTP/1.1 200 OK
+Content-Type: application/json
+Content-Length: 22
+Date: Thu, 22 Dec 2016 07:35:28 GMT
+Connection: close
+
+"Done duplicating app"
+```
+
+
+### Query Parameters
+```
+appName: string
+templateAppId: string
+ownerUserId: string
+```
+
+
+## Create a new app based on a template app, NOT replacing the load script of the new app
+When creating a new app that should keep the load script of the template app, use the /duplicateKeepScript endpoint.  
+The new app will be reloaded (using the load script from the template app) before saved to disk.
+  
+
+
+```sh
+curl -X GET "http://localhost:8000/duplicateKeepScript?appName=My%20new%20app&templateAppId=11111111-2222-3333-4444-555555555555&ownerUserId=joe"
+```
+
+Returns
+
+```
+HTTP/1.1 200 OK
+Content-Type: application/json
+Content-Length: 22
+Date: Thu, 22 Dec 2016 07:35:28 GMT
+Connection: close
+
+"Done duplicating app"
+```
+
+
+### Query Parameters
+```
+appName: string
+templateAppId: string
+ownerUserId: string
+```
+
+
+## Trying to create a new app based on an app that is not a designated template app
+
+
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ logger.transports.console.level = config.get('defaultLogLevel');
 logger.log('info', 'Starting Qlik Sense template app duplicator.');
 
 
-// Read certificates - Assuming certificates are in the working directory
+// Read certificates
 const client = fs.readFileSync(config.get('clientCertPath'));
 const client_key = fs.readFileSync(config.get('clientCertKeyPath'));
 
@@ -68,6 +68,7 @@ restServer.use(restify.queryParser());
 
 // Set up endpoints for REST server
 restServer.get('/duplicateNewScript', respondDuplicateNewScript);
+restServer.get('/duplicateKeepScript', respondDuplicateKeepScript);
 restServer.get('/getTemplateList', respondGetTemplateList);
 
 
@@ -218,6 +219,149 @@ function respondDuplicateNewScript(req, res, next) {
                                 logger.log('verbose', 'Setting load script...');
                                 app.setScript(loadScript);
                                 return app;
+                            })
+                            .then(app => {
+                                // Load the data
+                                logger.log('verbose', 'Reload app...');
+                                app.doReload();
+                                return app
+                            })
+                            .then(app => {
+                                // Save our data. Will persist to disk.
+                                logger.log('verbose', 'Save app to disk...');
+                                app.doSave();
+                                return;
+                            })
+                            .then(() => {
+                                // Close our connection.
+                                logger.log('verbose', 'Close connection to engine...');
+                                return globalEngine.connection.close();
+                            })
+                            .then(() => {
+                                logger.log('info', 'Done duplicating');
+                                res.send('Done duplicating app');
+                                next();
+                            })
+                            .catch(err => {
+                                // Failed to create app. In Desktop application names are unique.
+                                logger.log('error', 'Duplication error: ' + err);
+                                res.send(err);
+                                next(new restify.BadRequestError("Error occurred when test app template status 2."));;
+
+                            })
+                    }
+
+
+                })
+                .catch(err => {
+                    // Return error msg
+                    logger.log('error', 'Duplication error: ' + err);
+                    res.send(err);
+                    next(new restify.BadRequestError("Error occurred when test app template status."));;
+                    return;
+                })
+
+
+        }
+    });
+}
+
+
+// Handler for REST endpoint /duplicateKeepScript
+// URL parameters
+//   templateAppId: ID of app to use as template
+//   appName: Name of the new app that is created
+//   ownerUserId: User ID that should be set as owner of the created app
+function respondDuplicateKeepScript(req, res, next) {
+    var qrsInteractInstance = new qrsInteract(configQRS);
+
+    // Load script from git
+    request.get(loadScriptURL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            logger.log('verbose', 'Retrieved load script');
+
+            var loadScript = body;
+            logger.log('debug', 'Load script: ' + loadScript);
+
+            var newAppId = '';
+            var globalEngine = '';
+
+            var newOwnerId, newOwnerUserDirectory, newOwnerName;
+            var newOwnerUserId = req.params.ownerUserId;
+
+
+            // Make sure the app to be duplicated really is a template
+            qrsInteractInstance.Get('app/' + req.params.templateAppId)
+                .then(result => {
+                    logger.log('verbose', 'Testing if specifiec template app really is a template');
+
+                    var appIsTemplate = false;
+                    result.body.customProperties.forEach(function (item) {
+                        logger.log('debug', 'Item: ' + item);
+
+                        if (item.definition.name == 'AppIsTemplate' && item.value == 'Yes') {
+                            appIsTemplate = true;
+                        }
+                    })
+
+                    logger.log('verbose', 'App is template: ' + appIsTemplate);
+
+                    if (!appIsTemplate) {
+                        logger.log('warn', 'The provided app ID does not belong to a template app');
+                        next(new restify.InvalidArgumentError("The provided app ID does not belong to a template app."));
+                    }
+
+                    return appIsTemplate;
+                })
+                .then(result => {
+                    // result == true if the provided app ID belongs to a template app
+                    if (result) {
+
+                        qrsInteractInstance.Post('app/' + req.params.templateAppId + '/copy?name=' + req.params.appName, {}, 'json')
+                            .then(result => {
+                                sleep.sleep(5);
+                                logger.log('info', 'App created with ID %s, using %s as a template ', result.body.id, req.params.templateAppId);
+
+                                newAppId = result.body.id;
+                                // Get user details
+                                return qrsInteractInstance.Get("user?filter=userId eq '" + newOwnerUserId + "' and UserDirectory eq '" + config.get('senseUserDirectory') + "'");
+                            })
+                            .then(result => {
+                                // We have details of the user who should be made owner of the new app
+                                logger.log('debug', 'User details: ' + JSON.stringify(result.body));
+
+                                newOwnerId = result.body[0].id;
+                                newOwnerUserDirectory = result.body[0].userDirectory;
+                                newOwnerName = result.body[0].name;
+
+                                return qrsInteractInstance.Get('app/' + newAppId);
+                            })
+                            .then(result => {
+                                // We have a reference to the new app. Update its owner.
+                                // http://help.qlik.com/en-US/sense-developer/3.1/Subsystems/RepositoryServiceAPI/Content/RepositoryServiceAPI/RepositoryServiceAPI-Connect-API-Conflict-Handling.htm
+                                // http://help.qlik.com/en-US/sense-developer/3.1/Subsystems/RepositoryServiceAPI/Content/RepositoryServiceAPI/RepositoryServiceAPI-Update.htm
+                                logger.log('debug', 'Metadata of new app: ', JSON.stringify(result.body));
+
+                                var newBody = result.body;
+                                newBody.owner.id = newOwnerId;
+                                newBody.owner.userDirectory = newOwnerUserDirectory;
+                                newBody.owner.userId = newOwnerUserId;
+                                newBody.owner.name = newOwnerName;
+
+                                logger.log('debug', 'Modified metadata of new app: ', JSON.stringify(result.body));
+
+                                return qrsInteractInstance.Put("app/" + newAppId, newBody);
+                            })
+                            .then(() => {
+                                // Connect to engine
+                                logger.log('verbose', 'Connecting to engine...');
+                                return qsocks.Connect(configEngine);
+                            })
+                            .then(global => {
+                                // Connected. Open the newly created app
+                                logger.log('verbose', 'Opening app...');
+                                globalEngine = global;
+                                return global.openDoc(newAppId)
                             })
                             .then(app => {
                                 // Load the data
